3b1cd32729085443643846b3463b471931341723
Cristian Zambrano <cc.zambrano2471@gmail.com>
2021-02-28 18:58:59 -0500

    Add possible error

---
+++ src/test1.py
@@ -0,0 +1,15 @@
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+a = 'pruebaaaa'
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6zldg8h=k4e5tfe7@(q87(oahb3hfd(8-n8jrfcvz4a(v+2hr('
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
